1686bd9f39e77dc43e5e4500f34323ba93f5dbd2
Alan Palazzolo <alan.palazzolo@startribune.com>
2017-08-11 09:58:34 -0500

    Building draft full map.

--- app/index.js
+++ app/index.js
@@ -2,17 +2,66 @@
  * Main JS file for project.
  */
 
-// Define globals that are added through the config.json file, here like this:
-// /* global _ */
+/* global $, _, cartodb */
 'use strict';
 
 // Dependencies
-
-// Import local ES6 modules like this:
-//import utilsFn from './utils.js';
-
-// Or import libraries installed with npm like this:
-// import module from 'module';
+import utilsFn from './utils.js';
 
 // Setup utils function
-//let utils = utilsFn({ });
+let utils = utilsFn({ });
+
+// Add some classes depending on what is going on
+if (utils.isEmbedded()) {
+  $('body').addClass('embedded');
+}
+if (utils.query.flush) {
+  $('body').addClass('flush');
+}
+
+// Get info
+let defaultMapConfig = 'http://alan-strib.carto.com/api/v2/viz/804d8a61-85b2-4b58-8e07-aa014702698b/viz.json';
+drawMap(defaultMapConfig);
+
+// Draw map
+function drawMap(config) {
+  let options = {
+    shareable: false,
+    title: false,
+    description: false,
+    search: false,
+    cartodb_logo: false,
+    center_lat: 44.9463334,
+    center_lon: -93.1365967,
+    zoom: 10,
+    legends: true,
+    fullscreen: true,
+  };
+
+  cartodb.createVis('map', config, options)
+    .done((carto, layers) => {
+      let visual = layers[1];
+      window.map = carto.getNativeMap();
+
+      visual.setInteractivity('lud_2010, lud_2016');
+      //visual.setQuery('SELECT * FROM land_use_changed_2016 WHERE ch20102016 = \'residential-->industrial\'');
+    })
+    .on('error', error);
+}
+
+// Handle fullscreen parts
+function fullscreen() {
+  let header = $('header').outerHeight(true) || 0;
+  let footer = $('footer').outerHeight(true) || 0;
+
+  $('.fullscreen-minus-header').css('top', header);
+  $('.fullscreen-minus-footer').css('bottom', footer);
+}
+let throttledFullscreen = _.throttle(fullscreen, 200);
+$(window).on('resize', throttledFullscreen);
+throttledFullscreen();
+
+// Handle error
+function error(e) {
+  console.error(e);
+}
