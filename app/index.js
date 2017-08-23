8b5852289886ae7c3ea80b0d95ee74e3484aadf4
Alan Palazzolo <alan.palazzolo@startribune.com>
2017-08-23 16:29:45 -0500

    Draft search.

--- app/index.js
+++ app/index.js
@@ -164,6 +164,70 @@ function drawBoth() {
     if (map) {
       map.invalidateSize();
     }
+
+    // Handle search
+    let $searchToggle = $('.search-toggle');
+    let $searchToggleButton = $('.search-toggle button');
+    let $search = $('.search-bar');
+    let $searchForm = $('.search-bar form');
+    let $input = $('.search-bar input');
+    let $geolocate = $('.search-bar .geolocate');
+
+    // Geocode
+    function geocode(input) {
+      if (!input) {
+        return;
+      }
+
+      let url = 'https://search.mapzen.com/v1/search?' + [
+        'text=' + encodeURIComponent(input),
+        'boundary.rect.min_lon=-95.77',
+        'boundary.rect.max_lon=-91.04',
+        'boundary.rect.min_lat=43.46',
+        'boundary.rect.max_lat=46.46',
+        'api_key=mapzen-pGUThrG'
+      ].join('&');
+      window.fetch(url).then((response) => {
+        return response.json();
+      })
+        .then((response) => {
+          if (response && response.features && response.bbox && map) {
+            let b = response.bbox;
+            map.fitBounds([[b[1], b[0]], [b[3], b[2]]]);
+          }
+        })
+        .catch(error);
+    }
+
+    // Submit
+    $searchForm.on('submit', (e) => {
+      e.preventDefault();
+      geocode($input.val());
+    });
+
+    // Geolocate
+    $geolocate.on('click', (e) => {
+      e.preventDefault();
+      utils.geolocate((error, latLng) => {
+        map.setView(latLng, 16);
+      });
+    });
+
+    // Toggle
+    $searchToggleButton.on('click', (e) => {
+      e.preventDefault();
+      let opened = $searchToggle.hasClass('opened');
+
+      $searchToggle.removeClass(opened ? 'opened' : 'closed');
+      $searchToggle.addClass(opened ? 'closed' : 'opened');
+
+      $search.removeClass(opened ? 'opened' : 'closed');
+      $search.addClass(opened ? 'closed' : 'opened');
+
+      if (!opened) {
+        $input.focus();
+      }
+    });
   }
 
   // Handle change of views
